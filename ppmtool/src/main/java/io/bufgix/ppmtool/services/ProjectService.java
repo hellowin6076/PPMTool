b2ef4e14cd011d7229a7261787254bdfdf8082b3
lee.huimok <hellowin6076@gmail.com>
2021-05-27 23:54:30 +0900

    Find Project by Identifier

--- ppmtool/src/main/java/io/bufgix/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/io/bufgix/ppmtool/services/ProjectService.java
@@ -12,15 +12,24 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
-
     public Project saveOrUpdateProject(Project project){
         try{
             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
             return projectRepository.save(project);
-        }catch (Exception e){
-            throw new ProjectIdException("Project Id '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }catch (Exception e) {
+            throw new ProjectIdException("Project Id '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
+    }
+
+    public Project findProjectByIdentifier(String projectId) {
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIdException("Project Id '"+projectId+"' does not exist");
         }
 
-
+        return project;
     }
+
 }
