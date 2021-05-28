9861391948cdc1bedb06f41972f2833270ed195c
lee.huimok <hellowin6076@gmail.com>
2021-05-28 19:54:02 +0900

    Create Project and header components and Add Bootstrap

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import { Component } from "react";
 import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Dashboard />
       </div>
     );
