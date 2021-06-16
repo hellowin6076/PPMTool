6e45b108cf71d842476ae78db9f6cc39547336a5
lee.huimok <hellowin6076@gmail.com>
2021-06-16 21:58:32 +0900

    Finishi up with update vailidation and delte

--- ppmtool/src/main/java/io/bufgix/ppmtool/web/BacklogController.java
+++ ppmtool/src/main/java/io/bufgix/ppmtool/web/BacklogController.java
@@ -1,5 +1,6 @@
 package io.bufgix.ppmtool.web;
 
+import io.bufgix.ppmtool.domain.Project;
 import io.bufgix.ppmtool.domain.ProjectTask;
 import io.bufgix.ppmtool.services.MapValidationErrorService;
 import io.bufgix.ppmtool.services.ProjectTaskService;
@@ -56,5 +57,12 @@ public class BacklogController {
         ProjectTask updatedTask = projectTaskService.updateByProjectSequence(projectTask, backlog_id, pt_id);
 
         return new ResponseEntity<ProjectTask>(updatedTask, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{backlog_id}/{pt_id}")
+    public ResponseEntity<?> deleteProjectTask(@PathVariable String backlog_id, @PathVariable String pt_id){
+        projectTaskService.deletePTByProjectSequence(backlog_id, pt_id);
+
+        return new ResponseEntity<String>("Project Task "+pt_id+" was deleted successfully", HttpStatus.OK);
     }
 }
