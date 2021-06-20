11240e4936042c9e44f3a3f84d1d1b227ef6089c
lee.huimok <hellowin6076@gmail.com>
2021-06-20 14:33:23 +0900

    Finish AddProjectTask, handle errors

--- ppmtool-react-client/src/components/ProjectBoard/ProjectTask/AddProjectTask.js
+++ ppmtool-react-client/src/components/ProjectBoard/ProjectTask/AddProjectTask.js
@@ -23,6 +23,12 @@ class AddProjectTask extends Component {
     this.onSubmit = this.onSubmit.bind(this);
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
   onChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
@@ -46,6 +52,7 @@ class AddProjectTask extends Component {
 
   render() {
     const { id } = this.props.match.params;
+    const { errors } = this.state;
     return (
       <div className="add-PBI">
         <div className="container">
@@ -60,12 +67,17 @@ class AddProjectTask extends Component {
                 <div className="form-group m-1">
                   <input
                     type="text"
-                    className="form-control form-control-lg"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.summary,
+                    })}
                     name="summary"
                     placeholder="Project Task summary"
                     value={this.state.summary}
                     onChange={this.onChange}
                   />
+                  {errors.summary && (
+                    <div className="invalid-feedback">{errors.summary}</div>
+                  )}
                 </div>
                 <div className="form-group m-1">
                   <textarea
@@ -130,6 +142,11 @@ class AddProjectTask extends Component {
 
 AddProjectTask.propTypes = {
   addProjectTask: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired,
 };
 
-export default connect(null, { addProjectTask })(AddProjectTask);
+const mapStateToProps = (state) => ({
+  errors: state.errors,
+});
+
+export default connect(mapStateToProps, { addProjectTask })(AddProjectTask);
