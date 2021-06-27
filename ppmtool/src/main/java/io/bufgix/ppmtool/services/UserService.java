2d55e7697e68a1fd05fbf5a03889265e53f8200f
lee.huimok <hellowin6076@gmail.com>
2021-06-27 17:12:31 +0900

    Create User Object, Validation, Repository, Service

---
+++ ppmtool/src/main/java/io/bufgix/ppmtool/services/UserService.java
@@ -0,0 +1,14 @@
+package io.bufgix.ppmtool.services;
+
+import io.bufgix.ppmtool.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    
+}
