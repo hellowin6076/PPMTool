6b49813c94497eeda2549eeeca3143642a7586f2
lee.huimok <hellowin6076@gmail.com>
2021-07-08 23:45:00 +0900

    Lock operations to specific User(update)

--- ppmtool/src/main/java/io/bufgix/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/io/bufgix/ppmtool/services/ProjectService.java
@@ -25,8 +25,22 @@ public class ProjectService {
     private UserRepository userRepository;
 
     public Project saveOrUpdateProject(Project project, String username){
+        String projectIdentifier = project.getProjectIdentifier().toUpperCase();
+        // project.getId != null
+        // find by db -> null
+
+        if(project.getId() != null){
+            Project existingProject = projectRepository.findByProjectIdentifier(projectIdentifier);
+
+            if(existingProject != null && (!existingProject.getProjectLeader().equals(username))){
+                throw new ProjectNotFoundException("Project not found in your account");
+            }else if (existingProject == null){
+                throw new ProjectNotFoundException("Project with ID : '"+project.getProjectIdentifier()+"' cannot be updated because it doesn't exist");
+            }
+        }
+
         try{
-        	
+
         	User user = userRepository.findByUsername(username);
         	project.setUser(user);
         	project.setProjectLeader(user.getUsername());
