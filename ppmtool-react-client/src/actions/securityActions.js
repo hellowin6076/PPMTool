66f6037952bd08511ec4bec10aff612d2ae52b44
lee.huimok <hellowin6076@gmail.com>
2021-08-07 17:34:56 +0900

    SecurityActions, SecurityReducer and login form

--- ppmtool-react-client/src/actions/securityActions.js
+++ ppmtool-react-client/src/actions/securityActions.js
@@ -1,5 +1,7 @@
 import axios from "axios";
+import setJWTToken from "../securityUtils/setJWTToken";
 import { GET_ERRORS } from "./types";
+import jwt_decode from "jwt-decode";
 
 export const createNewUser = (newUser, history) => async (dispatch) => {
   try {
@@ -10,5 +12,30 @@ export const createNewUser = (newUser, history) => async (dispatch) => {
       type: GET_ERRORS,
       payload: err.response.data,
     });
+  }
+};
+
+export const login = (LoginRequest) => async (dispatch) => {
+  try {
+    // post => Login Request
+    const res = await axios.post("/api/users/login", LoginRequest);
+    // extratct token for res.data
+    const { token } = res.data;
+    // store the token in the localStorage
+    localStorage.setItem("jwtToken", token);
+    // set oure token in header ***
+    setJWTToken(token);
+    // decode token on React
+    const decoded = jwt_decode(token);
+    // dispatch to oure securityReducer
+    dispatch({
+      type: SET_CURRENT_USER,
+      payload: decoded,
+    });
+  } catch (err) {
+    //dispatch({
+    //  type: GET_ERRORS,
+    //  payload: err.response.data,
+    // });
   }
 };
